8d66c10469c7e84b12bf24a7e562a5f99cbf4883
VI <spelrai06@gmail.com>
2025-12-07 17:23:40 +0700

    add config | database

---
+++ backend/app/config.py
@@ -0,0 +1,19 @@
+from pydantic_settings import BaseSettings
+
+class Settings(BaseSettings):
+    app_name: str = "Bulkinskaya"
+    debug: bool = True
+    database_url: str = "sqllite:///./bulka.db"
+    cors_origins: list = [
+        "http://localhost:5173",
+        "http://localhost:8000",
+        "http://127.0.0.1:8000",
+        "http://127.0.0.1:5173",
+    ]
+    static_dir: str = "static"
+    images_dir: str = "static/images"
+
+    class Config:
+        env_file  = ".env"
+
+settings = Settings()
